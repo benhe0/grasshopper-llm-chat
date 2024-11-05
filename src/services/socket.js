c59c8182ac4a0f946d1084e0454db6a520623b36
Benjamin Herrmann <b.-herrmann@web.de>
2024-11-05 20:49:00 +0100

    params sync from grasshopper works

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,24 +1,49 @@
 import { io } from 'socket.io-client';
+import { reactive } from 'vue';
 
 const DEFAULT_URL = 'http://localhost:5001';
+
+export const connectionState = reactive({
+  connected: false,
+  error: null,
+});
 
 let socket = null;
 
 export function initSocket(url = DEFAULT_URL) {
-  // TODO: implement proper connection handling
-  socket = io(url);
+  if (socket) return socket;
+
+  socket = io(url, {
+    transports: ['websocket', 'polling'],
+    reconnection: true,
+    reconnectionAttempts: 10,
+  });
 
   socket.on('connect', () => {
-    console.log('Connected to server');
+    console.log('[Socket] Connected');
+    connectionState.connected = true;
+    connectionState.error = null;
   });
 
   socket.on('disconnect', () => {
-    console.log('Disconnected');
+    console.log('[Socket] Disconnected');
+    connectionState.connected = false;
+  });
+
+  socket.on('connect_error', (err) => {
+    console.error('[Socket] Connection error:', err);
+    connectionState.error = err.message;
   });
 
   return socket;
 }
 
 export function getSocket() {
+  if (!socket) return initSocket();
   return socket;
+}
+
+export function emitParamsUpdate(params) {
+  const s = getSocket();
+  s.emit('params_update', { params });
 }
